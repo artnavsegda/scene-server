5ee52ecb9948c563b2611a0288d68fa8b263afcd
Art Navsegda <artnavsegda@gmail.com>
2021-04-09 17:46:51 +0300

    Update media.js

--- media.js
+++ media.js
@@ -1,11 +1,14 @@
 import mqtt from "mqtt";
+import EventEmitter from "events";
 
 var client  = mqtt.connect('mqtt://127.0.0.1')
+
+const myEmitter = new EventEmitter();
 
 client.on('connect', function () {
     client.subscribe('presence', function (err) {
       if (!err) {
-        //client.publish('presence', 'Hello mqtt')
+        client.publish('presence', 'Hello mqtt')
       }
     })
   })
@@ -16,6 +19,19 @@ client.on('message', function (topic, message) {
     //client.end()
 })
 
+//shut down every unused source
+myEmitter.on('turn', function(power, location, source) {
+  if (power == "on")
+  {
+    ['kodi', 'appletv'].forEach((element) => {
+      if (element != source)
+      {
+        client.publish('/media/livingroom/'+ element +'/on', "0", {retain: true})
+      }
+    })
+  }
+});
+
 export function turn(parameters)
 {
   let timeout = 0;
@@ -23,6 +39,8 @@ export function turn(parameters)
   console.log(parameters.power);
   console.log(parameters.location);
   console.log(parameters.source);
+
+  myEmitter.emit('turn', parameters.power, parameters.location, parameters.source);
 
   if (parameters.power == "on")
   {
