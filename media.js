0634d14d0ce655d1037cd2abe66a16451fb06c7e
Art Navsegda <artnavsegda@gmail.com>
2021-08-24 11:46:43 +0300

    Update media.js

--- media.js
+++ media.js
@@ -46,9 +46,9 @@ let sources = {
 let rooms = {
   "cinema": { list: ["appletv", "kodi", "appletv2", "kodi2"], current: "", matrixjoin: 21},
   "livingroom": { list: ["appletv", "kodi", "yamaha_big", "appletv2", "kodi2", "smarttv"], current: "", matrixjoin: 21, tvjoin: { on: 100, off: 101, hdmi1: 103, tv: 109 }},
-  "kitchen": { list: ["yamaha", "yamaha2", "appletv", "kodi", "appletv2", "kodi2", "smarttv"], current: "", ampcode: 1, ampon: 11, ampoff: 21, matrixjoin: 22, tvjoin: { on: 200, off: 201, hdmi1: 203, tv: 209, smarthub: 221 }, ampinput: 11, matrixaudio: 31},
+  "kitchen": { list: ["yamaha", "yamaha2", "appletv", "kodi", "appletv2", "kodi2", "smarttv"], current: "", ampcode: 1, ampon: 11, ampoff: 21, matrixjoin: 22, tvjoin: { on: 200, off: 201, hdmi1: 203, tv: 209, smarthub: 221 }, ampinput: 11, matrixaudio: 31, tvinput: 3},
   "bathroom": { list: ["yamaha", "yamaha2"], current: "", ampcode: 2, ampon: 12, ampoff: 22},
-  "bedroom": { list: ["yamaha", "yamaha2", "appletv", "kodi", "appletv2", "kodi2", "smarttv"], current: "", ampcode: 3, ampon: 13, ampoff: 23, matrixjoin: 24, tvjoin: { on: 300, off: 301, hdmi1: 303, tv: 309 }, ampinput: 12, matrixaudio: 32},
+  "bedroom": { list: ["yamaha", "yamaha2", "appletv", "kodi", "appletv2", "kodi2", "smarttv"], current: "", ampcode: 3, ampon: 13, ampoff: 23, matrixjoin: 24, tvjoin: { on: 300, off: 301, hdmi1: 303, tv: 309, smarthub: 221 }, ampinput: 12, matrixaudio: 32, tvinput: 4},
   "kidsroom": { list: [], current: "", matrixjoin: 25},
   "bedroombathroom": { list: ["yamaha", "yamaha2"], current: "", ampcode: 4, ampon: 14, ampoff: 24},
   "highfloorbathroom": { list: ["yamaha", "yamaha2"], current: "", ampcode: 5, ampon: 15, ampoff: 25},
@@ -172,6 +172,9 @@ function powerOn(location, source, prevSource)
         case "appletv2":
         case "kodi2":
           cip.aset(rooms[location].ampcode, rooms[location].ampinput);
+          break;
+        case "smarttv":
+          cip.aset(rooms[location].ampcode, rooms[location].tvinput);
           break;
       }
     break;
