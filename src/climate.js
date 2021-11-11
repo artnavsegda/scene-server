df3d02db77abf2296f10f6b39e68918c19b447d4
Art Navsegda <artnavsegda@gmail.com>
2021-11-11 19:13:10 +0300

    Update climate.js

--- src/climate.js
+++ src/climate.js
@@ -1,9 +1,14 @@
-export function processDaily(elementList)
+export function processDaily(elementList, schedule)
 {
+    var setValue = schedule[new Date().getHours()] ? 1 : 0;
 
+    elementList.forEach(element => {
+        cip.dset(elementList[element], setValue);
+    });
 }
 
-export function processWeekly()
+export function processWeekly(elementList,schedule)
 {
-    
+    var dayWeekNumber = (new Date().getDay() + 6) % 7;
+    processDaily(elementList, schedule[dayWeekNumber]);
 }
